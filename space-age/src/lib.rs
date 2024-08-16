f30a55c8ebf2111e38a1ea9088ca6eba8ff54922
Dylan Jansen <dylanjansen1999@hotmail.com>
2024-08-16 12:15:05 +0200

    macro_rules!

---
+++ space-age/src/lib.rs
@@ -0,0 +1,46 @@
+// The code below is a stub. Just enough to satisfy the compiler.
+// In order to pass the tests you can add-to or change any of this code.
+
+#[derive(Debug)]
+pub struct Duration{
+    years: f64
+}
+
+impl From<u64> for Duration {
+    fn from(s: u64) -> Self {
+        Self{
+            years: s as f64 / 31_557_600.0
+        }
+    }
+}
+
+pub trait Planet {
+    const ORBITAL_PERIOD: f64;
+
+    fn years_during(d: &Duration) -> f64 {
+
+        d.years / Self::ORBITAL_PERIOD
+    }
+}
+
+macro_rules! define_planet {
+    ($($name:ident: $period:expr),+) => {
+        $(
+            pub struct $name;
+            impl Planet for $name {
+                const ORBITAL_PERIOD: f64 = $period;
+            }
+        )+
+    };
+}
+
+define_planet! {
+    Mercury: 0.2408467,
+    Venus: 0.61519726,
+    Earth: 1.0,
+    Mars: 1.8808158,
+    Jupiter: 11.862615,
+    Saturn: 29.447498,
+    Uranus: 84.016846,
+    Neptune: 164.79132
+}
