c23cf4378ee0bed714fdca69a786c924b8d87b5d
Dylan Jansen <dylanjansen1999@hotmail.com>
2024-08-27 21:23:42 +0200

    🍳

---
+++ eliuds-eggs/src/lib.rs
@@ -0,0 +1,8 @@
+pub fn egg_count(display_value: u32) -> usize {
+
+    let binary_string = format!("{:b}", display_value);
+    binary_string
+        .chars()
+        .filter(|&c| c == '1')
+        .count()
+}
