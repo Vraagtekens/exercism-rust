7e38c62efb686bb3068cf57baab9cdac4745a957
Dylan Jansen <dylanjansen1999@hotmail.com>
2024-09-06 22:16:49 +0200

    series

---
+++ series/src/lib.rs
@@ -0,0 +1,7 @@
+
+pub fn series(digits: &str, len: usize) -> Vec<String> {
+    todo!("What are the series of length {len} in string {digits:?}")
+
+    // let mut vec = Vec::new();
+    // digits.clone().chars().into_iter().
+}
