23712fd56dd521e099f10416a1634faebc841bc4
Dylan Jansen <dylanjansen1999@hotmail.com>
2024-09-08 22:33:10 +0200

    series

--- series/src/lib.rs
+++ series/src/lib.rs
@@ -1,7 +1,25 @@
 
 pub fn series(digits: &str, len: usize) -> Vec<String> {
-    todo!("What are the series of length {len} in string {digits:?}")
-
     // let mut vec = Vec::new();
-    // digits.clone().chars().into_iter().
+    // if len == 0 || len > digits.len() {
+    //     return vec;
+    // }
+
+    // // Generate substrings of specified length
+    // for i in 0..=(digits.len() - len) {
+    //     vec.push(digits[i..i + len].to_string());
+    // }
+
+    // vec
+
+
+    if len == 0 {
+        return vec!["".to_string(); digits.len() + 1];
+    }
+    digits
+        .chars()
+        .collect::<Vec<char>>()
+        .windows(len)
+        .map(|s| s.iter().collect::<String>())
+        .collect()
 }
