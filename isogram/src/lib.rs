a6a47d5732e9ba64db120b223c1cba7390568655
Dylan Jansen <dylanjansen1999@hotmail.com>
2024-09-09 20:18:52 +0200

    isogram

---
+++ isogram/src/lib.rs
@@ -0,0 +1,12 @@
+use std::collections::HashSet;
+
+pub fn check(candidate: &str) -> bool {
+
+    let mut seen = HashSet::new();
+    
+    candidate
+        .to_lowercase()
+        .chars()
+        .filter(|c| c.is_alphabetic())
+        .all(|c| seen.insert(c))
+}
