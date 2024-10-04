33ac7ecf00129beeeab108142437acb00aeb4e2a
Dylan Jansen <dylanjansen1999@hotmail.com>
2024-10-04 20:45:06 +0200

    accumulate

---
+++ accumulate/src/lib.rs
@@ -0,0 +1,9 @@
+
+
+/// What should the type of _function be?
+pub fn map<T, F, U>(input: Vec<T>, function: F) -> Vec<U>
+where
+    F: FnMut(T) -> U
+{
+    input.into_iter().map(function).collect()
+}
