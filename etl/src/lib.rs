8eaf63a6b2d40a58397f87fd9e03b121d919afe9
Dylan Jansen <dylanjansen1999@hotmail.com>
2024-10-20 18:35:53 +0200

    fizzy

--- etl/src/lib.rs
+++ etl/src/lib.rs
@@ -3,10 +3,9 @@ use std::collections::BTreeMap;
 pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
     let mut arr: BTreeMap<char, i32> = BTreeMap::new();
 
-    for x in h {
-        for c in x.1 {
-            let lower: char = c.to_lowercase().next().unwrap();
-            arr.insert(lower, *x.0);
+    for (a, b) in h {
+        for c in b {
+            arr.insert(c.to_ascii_lowercase(), *a);
         }
     }
 
