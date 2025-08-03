da34477c9a7d5090004a3c35213cb62b07a04689
Dylan Jansen <dylanjansen1999@hotmail.com>
2025-08-03 21:50:53 +0200

    nice

--- luhn-trait/src/lib.rs
+++ luhn-trait/src/lib.rs
@@ -7,8 +7,40 @@ pub trait Luhn {
 /// by hand for every other type presented in the test suite,
 /// but your solution will fail if a new type is presented.
 /// Perhaps there exists a better solution for this problem?
-impl Luhn for &str {
+impl<T: ToString> Luhn for T {
     fn valid_luhn(&self) -> bool {
-        todo!("Determine if '{self}' is a valid credit card number.");
+        let s = self.to_string();
+        let result: Option<u32> = s
+            .chars()
+            .filter(|c| !c.is_whitespace())
+            .rev()
+            .enumerate()
+            .map(|c| {
+                let digit = c.1.to_digit(10)?;
+                let i = c.0 + 1;
+                if i % 2 == 0 {
+                    let num = digit * 2;
+                    if num > 9 {
+                        return Some(num - 9);
+                    }
+                    return Some(num);
+                }
+
+                Some(digit)
+            })
+            .sum();
+
+        match result {
+            Some(sum) => sum % 10 == 0 && sum != 0,
+            None => false,
+        }
     }
 }
+
+// impl<T: ToString> From<T> for Luhn {
+
+//     fn from(input: T) -> Self {
+//         // todo!("From the given input '{input}' create a new Luhn struct.");
+//         Luhn(input.to_string())
+//     }
+// }
